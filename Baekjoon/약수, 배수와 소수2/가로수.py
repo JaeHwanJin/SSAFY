4cb466b193ef31b84c63d04148541a44c8e26152
진재환 <jjh099@icloud.com>
2023-03-28 17:52:14 +0900

    0328 알고리즘 다시시작

--- Baekjoon/약수, 배수와 소수2/가로수.py
+++ Baekjoon/약수, 배수와 소수2/가로수.py
@@ -32,9 +32,22 @@
 18
 예제 출력 2
 5
+
 '''
 import sys
+import math
 
 N = int(input())
-for n in range(N):
-    pass
+arr = [int(input()) for i in range(N)] # 심겨져있는 가로수
+arr2 = [] # 심겨져있는 가로수들의 거리
+
+for i in range(1,len(arr)):
+    arr2.append(arr[i] - arr[i-1])
+
+d = math.gcd(*arr2) # 심겨져 있는 가로수들의 거리의 최대공약수
+cnt = 0
+
+for j in arr2:
+    cnt += (j // d) -1
+print(cnt)
+
