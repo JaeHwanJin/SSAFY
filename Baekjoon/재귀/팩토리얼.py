f1aa40b12019783539127b5955517a1c2862cb46
진재환 <jjh099@icloud.com>
2023-03-29 17:53:13 +0900

    0329

--- Baekjoon/재귀/팩토리얼.py
+++ Baekjoon/재귀/팩토리얼.py
@@ -24,11 +24,7 @@
 def fibonacci(n):
     if n <= 1:
         return n
-    return fibonacci(n-1) + fibonacci(n-2)
-
-n = int(input())
-print(fibonacci(n))
+    return n * fibonacci(n - 1)
 
 
-
-print(fibonacci(int(input())))
+print(fibonacci(10))
