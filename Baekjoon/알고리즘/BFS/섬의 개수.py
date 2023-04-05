29231019908512c4222789d98f728456ae17c2e7
jinjaehwan <jjh099@icloud.com>
2023-04-05 23:42:24 +0900

    1

--- Baekjoon/알고리즘/BFS/섬의 개수.py
+++ Baekjoon/알고리즘/BFS/섬의 개수.py
@@ -50,34 +50,33 @@
 from collections import deque
 
 
-def BFS(a, b):
-    cnt = 1
-    mr = [0, 0, 1, -1]
-    mc = [1, -1, 0, 0]
+def BFS(i, j):
+    mr = [0, 0, 1, -1, 1, 1, -1, -1]
+    mc = [1, -1, 0, 0, 1, -1, -1, 1]
+    q = deque()
+    q.append((i, j))
+    MAP[i][j] = 0
     while q:
         x, y = q.popleft()
-        for k in range(4):
+        for k in range(8):
             nr = mr[k] + x
             nc = mc[k] + y
-            if 0 <= nr < h and 0 <= nc < w and MAP[nr][nc] == 1:
-                cnt = 1
-    return cnt
+            if 0 <= nr < h and 0 <= nc < w and MAP[nr][nc]:
+                MAP[nr][nc] = 0
+                q.append((nr, nc))
+
+
+
 
 while True:
     w, h = map(int, input().split())  # 지도의 너비와 높이
     if w == 0 and h == 0:
         break
     MAP = [list(map(int, input().split())) for _ in range(h)]
-    q = deque()
+    cnt = 0
     for i in range(h):
         for j in range(w):
             if MAP[i][j] == 1:
-                q.append((i, j))
-    print(q)
-    result= 0
-    for a in range(h):
-        for b in range(w):
-            if BFS(a, b) == 1:
-                result += 1
-    print(result)
-
+                BFS(i, j)
+                cnt += 1
+    print(cnt)
